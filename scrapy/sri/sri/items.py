2e807b75168826abf2d3f779b72e534971a13178
Ricardo Pelcastre <pelka.dev@gmail.com>
2023-11-06 17:06:38 -0600

    Edited SRI scrapper

--- scrapy/sri/sri/items.py
+++ scrapy/sri/sri/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class SriItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# --- Sri item
+class SriItem(Item):
+    full_name = Field()
+    cedula = Field()
+    message = Field()
+    firm_debts = Field()
+    disputed_debts = Field()
+    payment_facilities = Field()
--- scrapy/sri/sri/pipelines.py
+++ scrapy/sri/sri/pipelines.py
@@ -1,13 +1,55 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import re
+from decimal import Decimal
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
-class SriPipeline:
+# process fields
+def process_fields(item_adapter: ItemAdapter, fields_key: list[str], func: any, *args, **kwargs):
+    for field_key in fields_key:
+        value = item_adapter.get(field_key)
+        if value is not None:
+            item_adapter[field_key] = func(value, *args, **kwargs)
+
+
+def string_to_decimal(value: str, non_numeric: bool = False, splited_by_comma: bool = False):
+    if splited_by_comma:
+        splited_value = value.split(",")
+        value = ".".join(splited_value)
+
+    if non_numeric:
+        value = re.findall(
+            r"\d+\.\d+|\d+",
+            value,
+        )[0]
+
+    decimal_value = Decimal(value)
+    return str(decimal_value)
+
+
+# data formating & cleaning functions
+def strip_white_spaces(item_adapter: ItemAdapter):
+    field_names = item_adapter.field_names()
+    for field_name in field_names:
+        value = item_adapter.get(field_name)
+        if value is not None:
+            item_adapter[field_name] = value.strip()
+
+
+class PreprocesDataPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Strip all white spaces from strings
+        strip_white_spaces(adapter)
+
+        # Firm Debts,  Disputed Debts --> decimal
+        process_fields(
+            item_adapter=adapter,
+            fields_key=["firm_debts", "disputed_debts", "payment_facilities"],
+            func=string_to_decimal,
+            non_numeric=True
+        )
+
         return item
