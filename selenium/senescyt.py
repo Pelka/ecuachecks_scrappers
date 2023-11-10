0f9f30322924d0fed3dff6e8c58660f7db61646e
Ricardo Pelcastre <pelka.dev@gmail.com>
2023-11-10 10:07:41 -0600

    Added Senescyt

--- selenium/senescyt.py
+++ selenium/senescyt.py
@@ -1,100 +1,148 @@
+# develop library
+from pprint import pprint
+
+# Data processing
+from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, field_validator, Field
+from crawlab import save_item
 
+# Selenium
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
+# Utils for webscraping
 from fake_useragent import UserAgent
 import undetected_chromedriver as uc
 from undetected_chromedriver import ChromeOptions
-
-from selectolax.parser import HTMLParser, Node
 from twocaptcha import TwoCaptcha
 
+# Html parser
+from selectolax.parser import HTMLParser, Node
 
-class Degree(BaseModel):
+# CLI commands
+from click import command, option
+
+
+# Data handlers
+class PPDModel(BaseModel):
+    """
+    The PPDModel class provides common data cleaning operations for all fields string fields.
+
+    Methods:
+        strip_strings(v: Any): Strips whitespace from string values for all fields.
+        capitalize_words(v: Optional[str]): Capitalizes the first letter of each word in string values for all fields.
+    """
+
+    @field_validator('*')
+    @classmethod
+    def strip_strings(cls, value: any) -> any:
+        """
+        Strips whitespace from string if `value` is a string, otherwise returns `value` unchanged.
+        """
+        return value.strip() if isinstance(value, str) else value
+
+    @classmethod
+    def capitalize_words(cls, value: str) -> str:
+        """
+        Capitalizes the first letter of each word in a string if `value` is a string.
+        """
+        words = value.split()
+        capitalized_words = [word.capitalize() if len(
+            word) > 1 else word for word in words]
+        return ' '.join(capitalized_words)
+
+    @classmethod
+    def string_to_datetime(cls, value: str, date_format: str) -> datetime:
+        """
+        Converts a string to a datetime object according to the specified format.
+        """
+        return datetime.strptime(value, date_format)
+
+
+class Degree(PPDModel):
     title: str
     college: Optional[str]
     certificate_type: Optional[str]
     recognized: Optional[str]
     register_num: Optional[str]
-    register_date: Optional[str]
+    register_date: datetime
     area: Optional[str]
     note: Optional[str]
 
-    @field_validator('*')
-    @classmethod
-    def strip_strings(cls, V: any) -> str:
-        return V.strip() if isinstance(V, str) else V
-
     @field_validator('title', 'college', 'area', 'recognized', 'note')
     @classmethod
-    def capitalize_words(cls, V: str | None):
-        if V is not None:
-            words = V.split()
-            capitalized_words = [
-                word.capitalize() if len(word) > 1 else word for word in words
-            ]
-            V = ' '.join(capitalized_words)
-            return V
-        else:
-            return V
+    def capitalize_words(cls, value: str) -> str:
+        return super().capitalize_words(value)
+
+    @field_validator('register_date', mode="before")
+    @classmethod
+    def string_to_datetime(cls, value: str) -> datetime:
+        return super().string_to_datetime(value, "%Y-%m-%d")
 
 
-class EducationProfile(BaseModel):
+class EducationProfile(PPDModel):
     id_number: str
     full_name: str
     gender: str
     nacionality: str
     degress: list[Degree] = Field(default_factory=list)
 
-    @field_validator('*')
-    @classmethod
-    def strip_strings(cls, V: any) -> str:
-        return V.strip() if isinstance(V, str) else V
-
     @field_validator('gender', 'nacionality')
-    @classmethod
     def capitalize_words(cls, V: str):
-        if V is not None:
-            words = V.split()
-            capitalized_words = [
-                word.capitalize() if len(word) > 1 else word for word in words
-            ]
-            V = ' '.join(capitalized_words)
-            return V
-        else:
-            return V
+        return super().capitalize_words(V)
 
 
 class SenescytSpider:
     def __init__(self, search_id: str):
+        # Test id: 1721194593 1709026718 1709822207
         self.search_id = search_id
         self.target_url = "https://www.senescyt.gob.ec/consulta-titulos-web/faces/vista/consulta/consulta.xhtml;jsessionid=Fa3JjYFGiorrF4sr7TZQvZEKkMCJeLLQfYKjq8lS.srvprouioct26"
-        self.options = self._set_chrome_arguments([
-            "--headless",
-            "--disable-gpu",
-            f"--user-agent='{UserAgent().chrome}'",
-        ])
-        self.driver = self._init_driver(self.options)
-        self.driver.implicitly_wait(10)
+        self.driver = self._setup_driver()
 
-    def _set_chrome_arguments(self, arguments):
-        chrome_options = ChromeOptions()
-        for argument in arguments:
-            chrome_options.add_argument(argument)
-        return chrome_options
+    @classmethod
+    def start_from_cli(cls, search_id: str):
+        spider = cls(search_id)
+        status, data = spider.get_html()
 
-    def _init_driver(self, options):
-        service = Service()
-        return uc.Chrome(
-            service=service,
-            use_subprocess=True,
-            options=options
+        if status == "Ok":
+            item = spider.parse_data(data)
+            save_item(item.model_dump(mode='json'))
+            pprint(item.model_dump())
+        else:
+            spider.parse_error(data)
+
+    def _setup_driver(self):
+        # Setting Chrome options
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument('--disable-dev-shm-usage')
+
+        # Fake user agent
+        ua = UserAgent().random
+        options.add_argument(f"--user-agent={ua}")
+
+        # Setting up ChromeDriver
+        service = Service(
+            executable_path=ChromeDriverManager().install()
         )
+
+        driver = uc.Chrome(
+            service=service,
+            use_subprocess=False,
+            options=options,
+            # version_main=106
+        )
+
+        # Setting implitly wait time
+        driver.implicitly_wait(10)
+        return driver
 
     def _captcha_solver(self, img_src):
         solver = TwoCaptcha("331b57cff358c0e42f3529ab52c8409b")
@@ -108,54 +156,58 @@ class SenescytSpider:
 
         return result
 
-    def _wait_for_element(self, selector, timeout=10, by=By.CSS_SELECTOR):
-        return WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_element_located((by, selector))
+    def _wait_for_page_load(self, timeout: int = 10):
+        WebDriverWait(self.driver, timeout).until(
+            lambda d: d.execute_script(
+                'return document.readyState') == 'complete'
         )
 
     def get_html(self):
         self.driver.get(self.target_url)
 
-        img = self._wait_for_element(
-            'img[id="formPrincipal:capimg"]').get_attribute("src")
+        self._wait_for_page_load()
+
+        img = self.driver.find_element(
+            By.CSS_SELECTOR, 'img[id="formPrincipal:capimg"]'
+        ).get_attribute("src")
 
         result = self._captcha_solver(img)
 
+        if not result.get('code', ''):
+            raise Exception("Failed captcha")
+
+        self.driver.find_element(
+            By.CSS_SELECTOR, 'input[id="formPrincipal:identificacion"]'
+        ).send_keys(self.search_id)
+
+        self.driver.find_element(
+            By.CSS_SELECTOR, 'input[id="formPrincipal:captchaSellerInput"]'
+        ).send_keys(result.get('code'))
+
+        self.driver.find_element(
+            By.CSS_SELECTOR, 'button[id="formPrincipal:boton-buscar"]'
+        ).send_keys(Keys.ENTER)
+
+        self._wait_for_page_load()
+
         try:
-            if result.get('code', ''):
-                self._wait_for_element(
-                    'input[id="formPrincipal:identificacion"]'
-                ).send_keys(self.search_id)
-
-                self._wait_for_element(
-                    'input[id="formPrincipal:captchaSellerInput"]'
-                ).send_keys(result.get("code"))
-
-                self.driver.find_element(
-                    By.CSS_SELECTOR, 'button[id="formPrincipal:boton-buscar"]'
-                ).send_keys(Keys.ENTER)
-
-                try:
-                    self._wait_for_element('formPrincipal:j_idt39', 20, By.ID)
-                except:
-                    resp_msg = "Error"
-                else:
-                    resp_msg = "Ok"
-
-                resp_data = HTMLParser(self.driver.page_source)
-
-                return (resp_msg, resp_data)
-            else:
-                raise Exception("Failed captcha")
-
+            self.driver.find_element(
+                By.XPATH, '//div[contains(@id,"pnlListaTitulos") and contains(@class,"panel")]')
         except:
-            raise Exception("Couldn't interact with the page")
+            resp_msg = "Error"
+        else:
+            resp_msg = "Ok"
 
-        finally:
-            self.driver.quit()
+        resp_data = HTMLParser(self.driver.page_source)
+        self.driver.quit()
+
+        return (resp_msg, resp_data)
 
     def parse_error(self, tree: HTMLParser):
-        err_msg = tree.css_first('.msg-rojo').text().strip()
+        try:
+            err_msg = tree.css_first('.msg-rojo').text()
+        except:
+            err_msg = tree.css_first('.ui-messages-error').text()
         raise Exception(err_msg)
 
     def parse_data(self, tree: HTMLParser):
@@ -196,13 +248,10 @@ class SenescytSpider:
         return item
 
 
-if __name__ == "__main__":
-    spider = SenescytSpider(search_id="1721194593")
-    status, data = spider.get_html()
+@command()
+@option('--search_id', '-s', help="The id (cedula) to scrape")
+def cli(search_id):
+    SenescytSpider.start_from_cli(search_id)
 
-    if status == "Ok":
-        item = spider.parse_data(data)
-    else:
-        item = spider.parse_error(data)
 
-    print(item)
+cli()
