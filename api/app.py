8925228d10718a413868e39058e4c2dd1a6e6400
Ricardo Pelcastre <pelka.dev@gmail.com>
2023-11-18 20:04:07 -0600

    Added Web Socket API

--- api/app.py
+++ api/app.py
@@ -2,15 +2,20 @@ import json
 import asyncio
 import aiohttp
 import uuid
+from dataclasses import dataclass, field, asdict
+from typing import Optional
+import time
 
-from robyn import Robyn
-from robyn.robyn import Request, jsonify
-
-# TODO: handle data
+from robyn import Robyn, Request, WebSocket, WebSocketConnector, ALLOW_CORS, jsonify, Response
 # TODO: add auth
 
 app = Robyn(__file__)
+ALLOW_CORS(app, origins="GET, POST")
 
+BASE_URL = "http://5.161.59.45:8080/api"
+API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NDkyNWE3ZjhiZDEwY2ExMzI4ZWQwYyIsIm5iZiI6MTcwMDAyMzU3MCwidXNlcm5hbWUiOiJhZG1pbiJ9.XtOadzJ26oUztwaoLohn2n6FMkNFBsvyM6zjJ-3z5uA"
+
+global_tasks = {}
 scrappers = {
     "sri": "65498bddf8bd10ca1328ed83",
     "ant": "65498c25f8bd10ca1328ed85",
@@ -19,10 +24,29 @@ scrappers = {
     "min_educacion": "6557d002f8bd10ca1328f063"
 }
 
-scp_tasks = {}
 
-BASE_URL = "http://5.161.59.45:8080/api"
-API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NDkyNWE3ZjhiZDEwY2ExMzI4ZWQwYyIsIm5iZiI6MTcwMDAyMzU3MCwidXNlcm5hbWUiOiJhZG1pbiJ9.XtOadzJ26oUztwaoLohn2n6FMkNFBsvyM6zjJ-3z5uA"
+@dataclass
+class ScpTask:
+    id_crawlab: str
+    type: str
+    status: str
+
+
+@dataclass
+class GlobalTask:
+    success: bool
+    task_id: str
+    total_subtasks: Optional[int] = None
+    remaining_subtasks: Optional[int] = None
+    status: Optional[str] = None
+    error: Optional[str] = None
+    data: Optional[list[ScpTask]] = field(default_factory=list)
+
+    def is_completed(self):
+        return self.status == "completed"
+
+    def processed(self):
+        return self.success == "Ok"
 
 
 # TODO: handle exceptions
@@ -61,18 +85,6 @@ async def get_scraper_status(session: aiohttp.ClientSession, scp_task_id: str):
         return str(e)
 
 
-async def update_scrapper_status(session: aiohttp.ClientSession, scp: dict):
-    """
-    Updates the status of the scrapper in the task based on Crawlab response.
-    """
-
-    if scp["status"] == "running":
-        result = await get_scraper_status(session, scp["_id_crawlab"])
-        scp["status"] = result["data"]["status"]
-
-    return 0 if scp["status"] == "finished" or scp["status"] == "error" else 1
-
-
 async def get_scraper_data(session: aiohttp.ClientSession, scp_task_id: str):
     """
     Retrieves the data of a scraper task from Crawlab.
@@ -87,80 +99,137 @@ async def get_scraper_data(session: aiohttp.ClientSession, scp_task_id: str):
         return str(e)
 
 
-async def refine_scrapper_data(session: aiohttp, scp: dict):
+async def update_scrapper_status(session: aiohttp.ClientSession, scp: ScpTask):
+    """
+    Updates the status of the scrapper in the task based on Crawlab response.
+    """
+    result = await get_scraper_status(session, scp.id_crawlab)
+    scp.status = result["data"]["status"]
+    return scp if scp.status == "finished" or scp.status == "error" else None
+
+
+async def refine_scrapper_data(session: aiohttp, scp: ScpTask):
     """
     Return the refine data retrived by scrapper, based on Crawlab response.
     """
-    if scp["status"] == "finished":
-        result = await get_scraper_data(session, scp["_id_crawlab"])
-        json_result = json.loads(result)
-        return {"type": scp["type"], "data": json_result["data"]}
+    result = await get_scraper_data(session, scp.id_crawlab)
+    json_result = json.loads(result)
+    return {"type": scp.type, "data": json_result["data"]}
 
 
-@app.post("/scrappers/run/:search_id")
-async def scrappers_run(request: Request):
-    targets = request.queries.get("targets").split(",")
-    search_id = request.path_params.get("search_id")
-    task_id = uuid.uuid4().hex
+async def scrappers_run(session: aiohttp.ClientSession, global_tasks: dict, search_id: str, targets: list):
+    total_t = len(targets)
+    task_id = uuid.uuid1().hex
+    task = GlobalTask(
+        success=True,
+        status="running",
+        task_id=task_id,
+        total_subtasks=total_t,
+        remaining_subtasks=total_t
+    )
 
-    async with aiohttp.ClientSession() as session:
-        query_tasks = [
-            asyncio.create_task(
-                start_scrapping_task(session, search_id, target)
-            ) for target in targets
-        ]
-        results = await asyncio.gather(*query_tasks)
-
-    scp_tasks[task_id] = {
-        "total_tasks": len(targets),
-        "remaining_tasks": len(targets),
-        "scp_list": []
-    }
+    query_tasks = [
+        asyncio.create_task(
+            start_scrapping_task(session, search_id, target)
+        ) for target in targets
+    ]
+    results = await asyncio.gather(*query_tasks)
 
     for scp in results:
-        scp_data = {}
-        scp_data["_id_crawlab"] = scp["response"]["data"][0]
-        scp_data["type"] = scp["type"]
-        if scp["response"]["message"] == "success":
-            scp_data["status"] = "running"
-        else:
-            scp_data["status"] = "retrying"
+        scp_task = ScpTask(
+            id_crawlab=scp["response"]["data"][0],
+            type=scp["type"],
+            status="running" if scp["response"]["message"] == "success" else "error"
+        )
+        task.data.append(scp_task)
 
-        scp_tasks[task_id]["scp_list"].append(scp_data)
-
-    return jsonify({"status": "Ok", "id": task_id, "data": scp_tasks[task_id]})
+    global_tasks[task_id] = task
+    return task
 
 
-@app.get("/scrappers/status/:id")
-async def scrappers_status(request: Request):
-    task_id = request.path_params.get("id")
-    scp_task = scp_tasks[task_id]
-    scp_list = scp_task["scp_list"]
+async def scrappers_observer(search_id: str, targets: list):
+    global global_tasks
 
     async with aiohttp.ClientSession() as session:
-        query_task = [
-            asyncio.create_task(update_scrapper_status(session, scp)) for scp in scp_list
-        ]
-        result = await asyncio.gather(*query_task)
+        scp_task = await scrappers_run(session, global_tasks, search_id, targets)
+        yield asdict(scp_task)
 
-    scp_task["remaining_tasks"] = sum(result)
-    scp_tasks[task_id] = scp_task
-    return jsonify(scp_task)
+        while scp_task.remaining_subtasks > 0:
+            status_query = [
+                asyncio.create_task(
+                    update_scrapper_status(session, scp)
+                ) for scp in scp_task.data if not (scp.status == "finished" or scp.status == "error")
+            ]
+            status_res = await asyncio.gather(*status_query)
+            status_res = list(
+                filter(lambda item: item is not None, status_res))
+            finished_scp_tasks = len(status_res)
+            global_tasks[scp_task.task_id] = scp_task
+            if finished_scp_tasks > 0:
+                scp_task.remaining_subtasks = scp_task.remaining_subtasks - finished_scp_tasks
+
+                data_query = [
+                    asyncio.create_task(
+                        refine_scrapper_data(session, scp)
+                    ) for scp in status_res
+                ]
+                yield await asyncio.gather(*data_query)
+
+            time.sleep(3)
 
 
-@app.get("/scrappers/data/:id")
-async def scrappers_data(request: Request):
-    task_id = request.path_params.get("id")
-    scp_list = scp_tasks[task_id]["scp_list"]
+# Web Sockets
+websocket = WebSocket(app, "/scrappers")
 
-    async with aiohttp.ClientSession() as session:
-        query_task = [
-            asyncio.create_task(refine_scrapper_data(session, scp)) for scp in scp_list
-        ]
 
-        result = await asyncio.gather(*query_task)
+@websocket.on("connect")
+def message(ws: WebSocketConnector):
+    return jsonify({
+        "client_id": ws.id,
+        "socket_name": "scrappers",
+        "status": "connection established",
+    })
 
-    return jsonify(result)
+
+@websocket.on("message")
+async def connect(ws: WebSocketConnector, message: str):
+    json_mgs = json.loads(message)
+
+    if json_mgs["action"] == "run_follow":
+        search_id = json_mgs["params"]["search_id"]
+        targets = json_mgs["params"]["targets"]
+
+        async for res in scrappers_observer(search_id, targets):
+            ws.async_send_to(ws.id, jsonify(res))
+
+        return "tend"
+    else:
+        return jsonify({"error": "action not found"})
+
+
+@websocket.on("close")
+async def close():
+    return "Goodbye world, from ws"
+
+
+# @app.get("/scrappers/status/:id")
+
+
+# @app.get("/scrappers/data/:id")
+# async def scrappers_data(request: Request):
+#     task_id = request.path_params.get("id")
+#     scp_list = scp_tasks[task_id]["scp_list"]
+
+#     async with aiohttp.ClientSession() as session:
+#         query_task = [
+#             asyncio.create_task(refine_scrapper_data(session, scp)) for scp in scp_list
+#         ]
+
+#         result = await asyncio.gather(*query_task)
+
+#     result = list(filter(None, result))
+
+#     return jsonify(result)
 
 
 if __name__ == "__main__":
